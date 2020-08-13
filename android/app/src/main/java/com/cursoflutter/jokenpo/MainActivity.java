1b3e80cb3976aab682dfa28ed52eeab904a11425
PauloAlves8039 <pj38alves@gmail.com>
2020-08-12 22:44:39 -0300

    Versão inicial do projeto

---
+++ android/app/src/main/java/com/cursoflutter/jokenpo/MainActivity.java
@@ -0,0 +1,6 @@
+package com.cursoflutter.jokenpo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Jogo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// Classe responsável pelas ações do jogo.
+class Jogo extends StatefulWidget {
+  @override
+  _JogoState createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
